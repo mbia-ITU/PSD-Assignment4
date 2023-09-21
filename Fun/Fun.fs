d202e10c47feea6c92947020bfc26406278c8c9f
Mikkel Bistrup Andersen <mbia@itu.dk>
2023-09-21 11:58:39 +0200

    4.3 done and made fsharp project

--- Fun/Fun.fs
+++ Fun/Fun.fs
@@ -24,7 +24,7 @@ let rec lookup env x =
 
 type value = 
   | Int of int
-  | Closure of string * string * expr * value env       (* (f, x, fBody, fDeclEnv) *)
+  | Closure of string * (string list) * expr * value env       (* (f, x, fBody, fDeclEnv) *)
 
 let rec eval (e : expr) (env : value env) : int =
     match e with 
@@ -53,14 +53,14 @@ let rec eval (e : expr) (env : value env) : int =
       if b<>0 then eval e2 env
       else eval e3 env
     | Letfun(f, x, fBody, letBody) -> 
-      let bodyEnv = (f, Closure(f, x, fBody, env)) :: env 
+      let bodyEnv = (f, Closure(f, x, fBody, env)) :: env
       eval letBody bodyEnv
     | Call(Var f, eArg) -> 
       let fClosure = lookup env f
       match fClosure with
       | Closure (f, x, fBody, fDeclEnv) ->
-        let xVal = Int(eval eArg env)
-        let fBodyEnv = (x, xVal) :: (f, fClosure) :: fDeclEnv
+        let vals = List.map (fun (e, name) -> (name, Int(eval e env))) (List.zip eArg x)
+        let fBodyEnv = vals @ (f, fClosure) :: fDeclEnv
         eval fBody fBodyEnv
       | _ -> failwith "eval Call: not a function"
     | Call _ -> failwith "eval Call: not first-order function"
